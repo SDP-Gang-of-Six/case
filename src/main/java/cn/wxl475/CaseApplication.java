4ebe691448460096ab44e24a88f7c7aa0efdb169
wxl <1715142548@qq.com>
2024-03-06 20:39:28 +0800

    init

---
+++ src/main/java/cn/wxl475/CaseApplication.java
@@ -0,0 +1,11 @@
+package cn.wxl475;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CaseApplication.class,args);
+    }
+}
