ae931ae0d80f35ec9fbc536423d094972ee8953b
pc <2528578653@qq.com>
2024-03-15 17:23:41 +0800

    case模块bug修复

--- src/main/java/cn/wxl475/Service/impl/CaseServiceImpl.java
+++ src/main/java/cn/wxl475/Service/impl/CaseServiceImpl.java
@@ -4,7 +4,6 @@ package cn.wxl475.Service.impl;
 import cn.wxl475.Service.CaseService;
 import cn.wxl475.mapper.CaseMapper;
 import cn.wxl475.pojo.Case;
-import cn.wxl475.pojo.User;
 import cn.wxl475.redis.CacheClient;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/cn/wxl475/mapper/CaseMapper.java
+++ src/main/java/cn/wxl475/mapper/CaseMapper.java
@@ -1,12 +1,11 @@
 package cn.wxl475.mapper;
 
-import cn.wxl475.pojo.Case;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.*;
 
+import cn.wxl475.pojo.Case;
+import cn.wxl475.pojo.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class CaseMapper implements BaseMapper<Case> {
-
-
+public interface CaseMapper extends BaseMapper<Case> {
 }
